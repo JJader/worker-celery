fb1b3aa48d0269970610ca35b5de450f9605b5a2
jjader <jamisson.junior@a3data.com.br>
2024-08-18 18:06:18 -0300

    fix: fix result_backend to work inside docker-compose

--- src/celeryconfig.py
+++ src/celeryconfig.py
@@ -1,4 +1,5 @@
-broker_url = "pyamqp://guest@localhost//"
-result_backend = "db+postgresql://myuser:mypassword@localhost:5432/mydatabase"
+broker_url = "amqp://guest:guest@rabbitmq"
+# result_backend = "db+postgresql://myuser:mypassword@localhost:5432/mydatabase"
+result_backend = "db+postgresql://myuser:mypassword@db:5432/mydatabase"
 imports = ("app.mlflow",)
 result_extended = True
